c53f0c12c90a7e7b450ecdd21e4b84b999cd22eb
vagrant <vagrant@homestead>
2020-01-08 09:24:17 +0000

    as

--- database/migrations/2020_01_08_171245_create_categories_table.php
+++ database/migrations/2020_01_08_171245_create_categories_table.php
@@ -14,8 +14,10 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->increments('id');
+            $table->string('name')->index()->comment('名称');
+            $table->text('description')->nullable()->comment('描述');
+            $table->integer('post_count')->default(0)->comment('帖子数');
         });
     }
 
