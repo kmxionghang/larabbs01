9138eb5de57507f95658c30c1b8e52fc585aab22
php_xh <1364052798@qq.com>
2020-02-12 11:27:19 +0800

    新增话题

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -13,6 +13,10 @@ class TopicObserver
     {
         //
     }
+    public function saving(Topic $topic)
+    {
+        $topic->excerpt = make_excerpt($topic->body);
+    }
 
     public function updating(Topic $topic)
     {
